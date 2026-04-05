12dd75c9754a0e7ef6a2c89e305054b85398a71e
Eivind Larsen <to.eivind@gmail.com>
2026-04-05 21:27:58 +0200

    Update BackgroundPaintable.java

--- src/main/java/org/subsound/ui/components/BackgroundPaintable.java
+++ src/main/java/org/subsound/ui/components/BackgroundPaintable.java
@@ -4,20 +4,29 @@ import org.gnome.gdk.Paintable;
 import org.gnome.gdk.PaintableFlags;
 import org.gnome.gdk.Texture;
 import org.gnome.gobject.GObject;
+import org.jspecify.annotations.Nullable;
 
 import java.lang.foreign.Arena;
 import java.util.Set;
 
 public class BackgroundPaintable extends GObject implements Paintable {
-    private Texture currentTexture = null;
+    private @Nullable Texture currentTexture = null;
+
+    public void setTexture(@Nullable Texture texture) {
+        this.currentTexture = texture;
+        this.invalidateContents();
+    }
 
     @Override
     public void snapshot(org.gnome.gdk.Snapshot gdkSnapshot, double w, double h) {
+        if (currentTexture == null) {
+            return;
+        }
         try (var arena = Arena.ofConfined()) {
             float width = (float) w;
             float height = (float) h;
             var snapshot = (org.gnome.gtk.Snapshot) gdkSnapshot;
-            snapshot.pushOpacity(0.15f);
+            snapshot.pushOpacity(0.75f);
             snapshot.pushBlur(80.0f);
             currentTexture.snapshot(snapshot, width, height);
             snapshot.pop();
