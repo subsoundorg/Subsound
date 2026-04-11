b0b3540c43e6d212f7b8fa82ed43e27e8fb422e2
Eivind Siqveland Larsen <eivind@siqve.land>
2026-04-11 23:17:07 +0200

    cleanup build

--- src/main/resources/app/Subsound.sh
+++ src/main/resources/app/Subsound.sh
@@ -4,4 +4,4 @@
 # --enable-native-access=ALL-UNNAMED
 
 # We use `exec -a` in the Subsound.sh script to rename the process we start, so its not just named 'java' everywhere:
-exec -a Subsound java $JAVA_OPTS -jar /app/lib/Subsound-*.jar
+exec -a Subsound java $JAVA_OPTS -jar /app/lib/Subsound.jar
