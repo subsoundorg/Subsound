f55ec9ac3a3647517889df425c94be184a8af25b
Eivind Siqveland Larsen <eivind@siqve.land>
2026-04-11 23:25:47 +0200

    cleanup build

--- src/main/resources/app/Subsound.sh
+++ src/main/resources/app/Subsound.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-# TODO: add the needed $JAVA_OPTS args here?
-# --enable-native-access=ALL-UNNAMED
+export JAVA_OPTS="--enable-native-access=ALL-UNNAMED"
 
 # We use `exec -a` in the Subsound.sh script to rename the process we start, so its not just named 'java' everywhere:
-exec -a Subsound java $JAVA_OPTS -jar /app/lib/Subsound.jar
+exec -a Subsound java ${JAVA_OPTS} -jar /app/lib/Subsound.jar
