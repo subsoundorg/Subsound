21ccd97601e51dc2760d674080d076726d566e78
Eivind Siqveland Larsen <eivind@siqve.land>
2026-04-12 00:16:54 +0200

    cleanup build

--- src/main/resources/app/Subsound.sh
+++ src/main/resources/app/Subsound.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-export JAVA_OPTS="--enable-native-access=ALL-UNNAMED"
+JAVABIN=$JAVA_HOME/bin/java
+JAVA_OPTS="${JAVA_OPTS:+$JAVA_OPTS }--enable-native-access=ALL-UNNAMED"
 
 # We use `exec -a` in the Subsound.sh script to rename the process we start, so its not just named 'java' everywhere:
-exec -a Subsound java ${JAVA_OPTS} -jar /app/lib/Subsound.jar
+# JAVA_OPTS is intentionally unquoted for word splitting
+exec -a Subsound "${JAVABIN}" $JAVA_OPTS -jar /app/lib/Subsound.jar
